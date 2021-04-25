7022f6dc02a1d769c3f76fe32c9debb700507d0a
Wiktor123456789 <62619318+WiktorProgramista@users.noreply.github.com>
2021-04-25 20:51:18 +0200

    Update project1.py

--- project1.py
+++ project1.py
@@ -4,8 +4,8 @@ attempt = 0
 location1 = input("plik z ktorego chcesz wyszukac fraze: ")
 
 location2 = input("plik do ktorego chcesz wkleic: ")
-    
-with open(location1, "r", encoding='utf-8') as file1:
+
+with open(location1, "r", encoding='UTF-8') as file1:
     lines = file1.readlines()
 
     what = input("Wpisz co ma wyszukac w pliku: ")
@@ -15,8 +15,13 @@ with open(location1, "r", encoding='utf-8') as file1:
             if what in line:
                 file2.writelines(line)
                 print(line + "\n")
-            else:
-                print("Nie znaleziono ", what, "w pliku ", location1) 
+                attempt += 1
+                
+    if what in lines != lines:
+        print("Nie znaleziono ", what, "w pliku: ", location1)
+
+print("Znalezione wyniki: ", attempt)
+
 
 
 
