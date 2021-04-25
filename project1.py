1d5d83b415f9d7baab507441a936c91857fb3c9c
Wiktor123456789 <62619318+WiktorProgramista@users.noreply.github.com>
2021-04-25 16:38:58 +0200

    Update project1.py

--- project1.py
+++ project1.py
@@ -8,7 +8,7 @@ location2 = input("plik do ktorego chcesz wkleic: ")
 with open(location1, "r", encoding='utf-8') as file1:
     lines = file1.readlines()
 
-    what = input("Wpisz co ma wyszukac w pliku")
+    what = input("Wpisz co ma wyszukac w pliku: ")
 
     with open(location2, "w") as file2:
         for line in lines:
